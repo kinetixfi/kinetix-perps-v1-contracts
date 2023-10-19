a01b2e657bafe002876f79b3902a6a02a05f50f4
0xMattAxel <>
2023-10-19 15:17:11 +0300

    Tokenomics removed from this repo

--- contracts/tokenomics/interfaces/IV2Factory.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.5.0;
-
-interface IV2Factory {
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-}
